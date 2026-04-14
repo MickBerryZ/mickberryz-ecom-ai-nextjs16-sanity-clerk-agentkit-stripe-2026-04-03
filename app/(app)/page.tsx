90563d61cea8bd1d3576a38c70f32b4f50e3fbf3
mickberryz <sa.thawatchai@gmail.com>
2026-04-14 18:20:59 +0100

    feat: initial commit

---
+++ app/(app)/page.tsx
@@ -0,0 +1,11 @@
+export default function home() {
+  return (
+    <div>
+      {/* Featrured Products Carousel */}
+      {/* Page Banner */}
+      {/* Category Tiles */}
+      {/* Products Section */}
+      <h1>Hello World</h1>
+    </div>
+  );
+}
