5ef34a3db51a2e8bb68951626bf2693d9f048af9
mickberryz <sa.thawatchai@gmail.com>
2026-04-15 19:57:40 +0100

    import file (customerType, categoryType, productType, orderType, ) into sanity/schemaTypes

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,13 +1,14 @@
-import { Button } from "@/components/ui/button";
-
 export default function home() {
   return (
     <div>
       {/* Featrured Products Carousel */}
 
       {/* Page Banner */}
+
       {/* Category Tiles */}
+
       {/* Products Section */}
+
       <h1>Hello World</h1>
     </div>
   );
