1f7e091fa45b718e5eb31c0c4a41713cfce6c450
mickberryz <sa.thawatchai@gmail.com>
2026-04-15 20:17:41 +0100

    test sanityFetch data

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,14 @@
-export default function home() {
+import { sanityFetch } from "@/sanity/lib/live";
+
+export default async function home() {
+  const categoties = await sanityFetch({
+    query: `*[_type == "category"]`,
+  });
+
+  console.log("Categories:", categoties);
+
   return (
-    <div>
+    <div className="">
       {/* Featrured Products Carousel */}
 
       {/* Page Banner */}
