90dc459266d40ff164d940d2459d742d16ab992e
mickberryz <sa.thawatchai@gmail.com>
2026-05-12 18:47:17 +0100

    Checkpoint: Before total package reinstall - Clerk v6 error state

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,17 +1,31 @@
+import { Header } from "@/components/Header";
+import { ClerkClientProvider } from "@/components/providers/ClerkClientProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
 import { SanityLive } from "@/sanity/lib/live";
-import { ClerkProvider } from "@clerk/nextjs";
+// import { ClerkProvider } from "@clerk/nextjs";
 
 function Layout({ children }: { children: React.ReactNode }) {
+  console.log(
+    "DEBUG - Clerk Key:",
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+      ? "Founddddddddddddddddddddddd"
+      : "MISSINGgggggggggg",
+  );
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
+    <ClerkClientProvider>
       <CartStoreProvider>
-        <main>{children}</main>
-        <Toaster className="bottom-center" />
-        <SanityLive />
+        <ChatStoreProvider>
+          <Header />
+          <main>{children}</main>
+          <Toaster position="bottom-center" />
+          <SanityLive />
+        </ChatStoreProvider>
       </CartStoreProvider>
-    </ClerkProvider>
+    </ClerkClientProvider>
+    // </ClerkProvider>
   );
 }
 
